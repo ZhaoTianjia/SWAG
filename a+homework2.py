7b419d46f3d4ec07654669837b0dc54f15f0c94c
Zhao Tianjia <tianjiazhaox@gmail.com>
2021-01-01 23:23:52 +1100

    what a enjoyable day

--- a+homework2.py
+++ a+homework2.py
@@ -15,4 +15,55 @@
 # print(content)
 # file = open('a.csv', 'w')
 # file.write(content)
-print ('\a')
+# print ('\a')
+# str = 'swaggoose'
+# print(str[0:5])
+# print(str[0:9:2])
+# print(str[:5])
+# print(str[5:])
+#
+# str = "The administrative officer will report to the Director"
+# print(str.split(' '))
+testlist=[1,2,3,'test','list']
+# for i in testlist:
+#     print (i)
+# print (type(testlist[2]))
+# length= len(testlist)
+# i=0
+# while i< length:
+#     print (testlist[i])
+#     i=i+1
+# test=input("input a testwprd  ")
+# testlist.append(test)
+# for i in testlist:
+#     print (i)
+# testlist2=[4,5]
+# testlist.extend(testlist2)
+# testlist.append(testlist2)
+# print (testlist)
+# testlist.insert(2,7)
+# print(testlist)
+# del testlist[2]
+# print(testlist)
+# print(1 == '1')
+# a=[1,1,2,2,4,5,6,7,8]
+# print(a. index(1,0,4))
+# print (a.count(5))
+# a = [[], [66,654,33], []]
+# a[1].append(1)
+# # a[1]=[1]
+# print(a) # [[], [1], []]
+office=[[],[],[]]
+names=['a','b','c','d','e','f','g']
+import random
+for name in names:
+    b=random.randint(0,2)
+    office[b].append(name)
+    # if b == 1:
+    #     office[0].append(name)
+    # if b == 2:
+    #     office[1].append(name)
+    # if b == 3:
+    #     office[2].append(name)
+print(office)
+
---
+++ suffering.py
@@ -0,0 +1,30 @@
+products=[["iphone",6888],["mac",14800],["xiaomi",2499],["coffee",30],["book",60],["nike",699]]
+print('----- product list -----')
+for product, price in products:
+    print (product,'',price)
+
+# trolly=[]
+#
+# while True:
+#     b=input('choose what you want     ')
+#     if b== 'q':
+#         break
+#     choice = int(b)
+#     print(choice)
+#     if choice>5:
+#         print ('no such choice')
+#         continue
+#     trolly.append(products[choice][0])
+#     print (trolly)
+# print(trolly)
+# print(trolly)
+# info={'name':'swag','age':'18'}
+# print(info['name'])
+# print(info.get('gender','male'))
+# newid=input('please inpu your id ')
+# info['id']=newid
+# print (info)
+# for key, value in info.items():
+#     print(key, value)
+# mylist=['a','b','c','d']
+# print(list(info.items()))
